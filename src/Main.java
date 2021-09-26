6ab7ca4538ed4cc2b48bb134d1bb3c861e4b1dfa
Кузнецов Максим <maximkuznetsov17250@gmail.com>
2021-09-26 20:10:03 +0300

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticket = 12_000;
+        int mileRub = 20;
+        long mile = ticket / mileRub;
+        System.out.println(mile);
+    }
+}
